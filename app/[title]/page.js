5be6c7ec64c0b9ef68dcdabcadec3fec8955677a
brad-link <bradlinklater1@gmail.com>
2024-03-29 09:03:43 +0000

    check if json loading deployment error fixed

--- app/[title]/page.js
+++ app/[title]/page.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Quiz from './components/quiz';
 import loadQuizData from '../components/loadQuizData';
 import styles from './page.module.css';
+import { promises as fs } from 'fs';
 
 
 export default async function QuizPage({ params }) {
   const title = decodeURIComponent(params.title);
-  const quiz = await loadQuizData(title);
+  // const quiz = await loadQuizData(title);
+  const file = await fs.readFile(process.cwd() + '/app/questions.json', 'utf-8');
+  const quizData = JSON.parse(file);
+  const quiz = quizData[title];
   console.log(title);
 
 
